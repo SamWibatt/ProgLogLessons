f81bc6f49dba74d216bf49ac7231646e1c754b21
Sean Igo <samwibatt@gmail.com>
2019-12-21 11:37:11 -0800

    CSV handling improved a pinch

--- PL_L5_Optimize/CSV2Logic.py
+++ PL_L5_Optimize/CSV2Logic.py
@@ -7,15 +7,21 @@ if __name__ == "__main__":
         print("Takes a CSV file representing a truth table from multiple inputs to multiple outputs")
         print("Emits verilog code to enact that logic")
         sys.exit(1)
+
+
+    # right so here is the basic code from https://realpython.com/python-csv/#reading-csv-files-with-csv which I will
+    # hack up into what I want.
     infile = sys.argv[1]
     with open(infile) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_reader = csv.reader(csv_file, delimiter=',',skipinitialspace=True)
         line_count = 0
-        for row in csv_reader:
+
+        for quotedrow in csv_reader:
+            row = [col.strip('"') for col in quotedrow]
             if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
+                print(f'Column names are {"|".join(row)}')
                 line_count += 1
             else:
-                print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                print("|".join(row))
                 line_count += 1
         print(f'Processed {line_count} lines.')
